2948fb49fc85f6c0decf0332e2852e9f4cf6c696
James <james.fernandez1239@gmail.com>
2025-09-25 03:01:32 +0800

    adjusted the path for the announcement

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from core import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('announcements_list/', views.announcements_list, name='announcements_list'),
-    path('announcements_detail/<int:id>/', views.announcements_detail, name='announcements_detail'),
+    path('announcements/', views.announcements_list, name='announcements_list'),
+    path('announcements/<int:id>/', views.announcements_detail, name='announcements_detail'),
 ]
